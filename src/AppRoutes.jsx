6b9a9f140aa2a9b6267e5633712e245241900e31
Fibin Pious <fibin404@gmail.com>
2025-02-05 13:52:40 -0800

    updated router for netlify to /

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,21 +1,19 @@
 // src/routes/Routes.jsx
 import React from 'react';
-import { Route, Router, Routes } from 'react-router-dom'; // Use Routes instead of Switch
+import { Route, Routes } from 'react-router-dom'; // Use Routes instead of Switch
 import HomePage from './Components/HomePage';  // Adjust the import path as needed
 import CoursesSection from './Components/CoursesSection'  // Adjust the import path as needed
 import Login from './Components/Login'  // Adjust the import path as needed
-import AboutUs from './Components/AboutUs';
+import AboutUs from './Components/AboutUs'  // Adjust the import path as needed
 
 const AppRoutes = () => {
   return (
-    <Router basename='/'>
-      <Routes>
-        <Route path="/" element={<Login />} />  {/* Use 'element' prop with JSX */}
-        <Route path="/home" element={<HomePage />} />  {/* Use 'element' prop with JSX */}
-        <Route path="/about" element={<AboutUs />} />  {/* Use 'element' prop with JSX */}
-        <Route path="/courses" element={<CoursesSection />} />  {/* Use 'element' prop with JSX */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />  {/* Use 'element' prop with JSX */}
+      <Route path="/home" element={<HomePage />} />  {/* Use 'element' prop with JSX */}
+      <Route path="/about" element={<AboutUs />} />  {/* Use 'element' prop with JSX */}
+      <Route path="/courses" element={<CoursesSection />} />  {/* Use 'element' prop with JSX */}
+    </Routes>
   );
 };
 
